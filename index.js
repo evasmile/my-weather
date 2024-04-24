36b14b532c79f5b7d519e07f9bc43feca95a4038
surprise_Mathonsi <surprisehlawutelo@gmail..com>
2024-04-24 19:38:34 +0200

    add

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from'express';
+import bodyParser from "body-parser";
+import axios from 'axios';
+
+
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",function(req,res){
+    res.render("home.ejs")
+})
+
+app.post("/", async function(req,res){
+   
+    try{
+
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=7791aeab6c3b7a623eee7c764078479f`)
+
+        console.log(result.data)
+        res.render('home.ejs',{'data':result.data})
+    }catch(error){
+
+       console.log(error)
+    }
+})
+
+ async function getWeather(city){
+
+   
+}
+
+
+
+app.listen("3000",function(){
+   console.log("on")
+})
+
+      
+    
+
